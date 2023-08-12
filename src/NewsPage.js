a138af719710247c836b33632cbf168c078bffe6
King's meow <mingee641@gmail.com>
2023-08-12 22:02:50 +0900

    [ADD] News 페이지 및 기능 추가

---
+++ src/NewsPage.js
@@ -0,0 +1,21 @@
+import Categories from "./NewsComponent/Categories";
+import { useParams } from "react-router-dom"; // useParams를 가져옴
+import NewsList from "./NewsComponent/NewsList";
+
+const NewsPage = () => {
+  // useParams 훅을 사용하여 category 값을 추출
+  const { category } = useParams();
+
+  // 카테고리가 선택되지 않았으면 기본값 all로 사용
+  const selectedCategory = category || "all";
+
+  return (
+    <div>
+      <h1>News Page</h1>
+      <Categories />
+      <NewsList category={selectedCategory} />
+    </div>
+  );
+};
+
+export default NewsPage;

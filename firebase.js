04e5067fec0fa04e7241f35ce62e4a93a03d358d
King's meow <mingee641@gmail.com>
2023-08-12 22:01:16 +0900

    [ADD] firebase 설정

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore/lite'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
+};
+
+// firebaseConfig 정보로 firebase 시작
+const app = initializeApp(firebaseConfig);
+
+// firebase의 firestore 인스턴스를 변수에 저장
+const db = getFirestore(app);
+
+export default db; // Firestore 인스턴스를 내보냅니다.
